d3dc22c20075db931b434fcdd769a9144a930e98
Tera <24725862+teraa@users.noreply.github.com>
2020-12-07 11:21:10 +0100

    add day 7

--- Day07.a/Program.cs
+++ Day07.a/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
-namespace Day07.a
+const string Wanted = "shiny gold";
+
+var regex = new Regex(@"(?<parent>.*?) bags contain (?:(?<child>\d+ .*?) bags?(?:\.|, ))*", RegexOptions.Compiled);
+var dict = new Dictionary<string, Dictionary<string, int>>();
+string? line;
+while ((line = Console.ReadLine()) is not null)
 {
-    class Program
+    var match = regex.Match(line);
+    var parentName = match.Groups["parent"].Value;
+    var d = new Dictionary<string, int>();
+    dict[parentName] = d;
+
+    var children = match.Groups["child"].Captures;
+    foreach (Capture cc in children)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        var parts = cc.Value.Split(' ', 2);
+        var count = int.Parse(parts[0]);
+        var name = parts[1];
+        d[name] = count;
     }
 }
+
+bool Predicate(string name)
+{
+    var subDict = dict[name];
+    return subDict.Keys.Any(x => x == Wanted || Predicate(x));
+}
+
+var result = dict.Count(pair => Predicate(pair.Key));
+
+Console.WriteLine(result);
--- Day07.b/Program.cs
+++ Day07.b/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
-namespace Day07.b
+const string Wanted = "shiny gold";
+
+var regex = new Regex(@"(?<parent>.*?) bags contain (?:(?<child>\d+ .*?) bags?(?:\.|, ))*", RegexOptions.Compiled);
+var dict = new Dictionary<string, Dictionary<string, int>>();
+string? line;
+while ((line = Console.ReadLine()) is not null)
 {
-    class Program
+    var match = regex.Match(line);
+    var parentName = match.Groups["parent"].Value;
+    var d = new Dictionary<string, int>();
+    dict[parentName] = d;
+
+    var children = match.Groups["child"].Captures;
+    foreach (Capture cc in children)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        var parts = cc.Value.Split(' ', 2);
+        var count = int.Parse(parts[0]);
+        var name = parts[1];
+        d[name] = count;
     }
 }
+
+int Sum(string name)
+{
+    var subDict = dict[name];
+    return subDict.Sum(pair => pair.Value * (Sum(pair.Key) + 1));
+}
+
+int result = Sum(Wanted);
+
+Console.WriteLine(result);
