5662771dc9f6372d1127bff8011e6606d3bccf6a
Tera <24725862+teraa@users.noreply.github.com>
2020-12-10 12:14:23 +0100

    add day 10

---
+++ Day10.a/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+const int MaxDifference = 3;
+
+var list = new List<int>();
+string? line;
+while ((line = Console.ReadLine()) is not null)
+    list.Add(int.Parse(line));
+
+
+list.Sort();
+
+int[] differences = new int[MaxDifference];
+int previous = 0;
+for (int i = 0; i < list.Count; i++)
+{
+    var current = list[i];
+    differences[current - previous - 1]++;
+    previous = current;
+}
+
+Console.WriteLine(differences[0] * (differences[2] + 1));
