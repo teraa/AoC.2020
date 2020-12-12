f7d925a972680d27b6edfa70f849a00def1bc759
Tera <24725862+teraa@users.noreply.github.com>
2020-12-12 13:48:08 +0100

    add day 12

---
+++ Day12.a/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+const string directions = "NESW";
+int dir = 1;
+(int lat, int lon) ship = (0, 0);
+string? line;
+while ((line = Console.ReadLine()) is not null)
+{
+    char action = line[0];
+    var value = int.Parse(line[1..]);
+    switch (action)
+    {
+        case 'L':
+            dir = (dir + 4 - value / 90) % 4;
+            break;
+        case 'R':
+            dir = (dir + value / 90) % 4;
+            break;
+        case 'F':
+            action = directions[dir];
+            goto default;
+        default:
+            ship = action switch
+            {
+                'N' => (ship.lat + value, ship.lon),
+                'S' => (ship.lat - value, ship.lon),
+                'E' => (ship.lat, ship.lon + value),
+                'W' => (ship.lat, ship.lon - value),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        break;
+    }
+}
+
+Console.WriteLine(Math.Abs(ship.lat) + Math.Abs(ship.lon));
---
+++ Day12.b/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+var waypoint = new Point(1, 10);
+var ship = new Point(0, 0);
+string? line;
+while ((line = Console.ReadLine()) is not null)
+{
+    var action = line[0];
+    var value = int.Parse(line[1..]);
+    switch (action)
+    {
+        case 'L': waypoint = Rotate(waypoint, -value); break;
+        case 'R': waypoint = Rotate(waypoint, value); break;
+        case 'F': ship = new Point
+            (
+                Lat: ship.Lat + waypoint.Lat * value,
+                Lon: ship.Lon + waypoint.Lon * value
+            );
+            break;
+        default:
+            waypoint = action switch
+            {
+                'N' => new Point(waypoint.Lat + value, waypoint.Lon),
+                'S' => new Point(waypoint.Lat - value, waypoint.Lon),
+                'E' => new Point(waypoint.Lat, waypoint.Lon + value),
+                'W' => new Point(waypoint.Lat, waypoint.Lon - value),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        break;
+    }
+}
+
+Console.WriteLine(Math.Abs(ship.Lat) + Math.Abs(ship.Lon));
+
+Point Rotate(Point point, int degrees)
+{
+    var rad = (Math.PI / 180) * degrees;
+    var cos = (int)Math.Cos(rad);
+    var sin = (int)Math.Sin(rad);
+    var lat = point.Lat * cos - point.Lon * sin;
+    var lon = point.Lat * sin + point.Lon * cos;
+    return new Point(lat, lon);
+}
+
+record Point(int Lat, int Lon);
