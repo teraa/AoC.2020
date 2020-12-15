ea9b9cf33b1704852ac22aa707d4e1fa062a06f4
Tera <24725862+teraa@users.noreply.github.com>
2020-12-15 13:18:53 +0100

    update day15

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -5,29 +5,27 @@ using System.Linq;
 const int N = 2020;
 const int M = 30000000;
 
-var dict = new Dictionary<int, List<int>>();
+var dict = new Dictionary<int, (int, int)>();
 var starting = Console.ReadLine()!
     .Split(',')
     .Select(int.Parse)
     .ToArray();
 
 for (int i = 0; i < starting.Length; i++)
-    dict[starting[i]] = new() { i + 1 };
+    dict[starting[i]] = (i + 1, -1);
 
 int last = starting[^1];
 for (int i = dict.Count + 1; i <= M; i++)
 {
     var indexes = dict[last];
 
-    if (indexes.Count == 1)
-        last = 0;
-    else
-        last = indexes[^1] - indexes[^2];
+    last = indexes.Item2 == -1
+        ? 0
+        : indexes.Item1 - indexes.Item2;
 
-    if (dict.TryGetValue(last, out indexes))
-        indexes.Add(i);
-    else
-        dict[last] = new() { i };
+    dict[last] = dict.TryGetValue(last, out indexes)
+        ? (i, indexes.Item1)
+        : (i, -1);
 
     if (i is N or M)
         Console.WriteLine(last);
