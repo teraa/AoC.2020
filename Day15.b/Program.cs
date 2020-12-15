e4a39ab4f3c1c91832161e3aadf218d4a2e6e3c6
Tera <24725862+teraa@users.noreply.github.com>
2020-12-15 21:43:50 +0100

    add alternative solution

---
+++ Day15.b/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+const int N = 2020;
+const int M = 30000000;
+
+var starting = Console.ReadLine()!
+    .Split(',')
+    .Select(int.Parse)
+    .ToArray();
+
+var arr = new int[M];
+int i = 0;
+while (i < starting.Length)
+    arr[starting[i]] = ++i;
+
+int n = 0;
+do
+{
+    var idx = arr[n];
+    var next = idx == 0 ? 0 : i - idx;
+
+    arr[n] = i++;
+    n = next;
+
+    if (i is N or M)
+        Console.WriteLine(n);
+} while (i < M);
